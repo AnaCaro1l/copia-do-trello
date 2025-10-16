77ca8ad7b9553ab5a2bcda3477328c93115bc8e5
Ana Caroll <ana2417carollina@gmail.com>
2025-10-16 13:47:47 -0300

    feat(front): criando novo quadro

--- src/app/components/default-card/default-card.component.ts
+++ src/app/components/default-card/default-card.component.ts
@@ -3,29 +3,40 @@ import { MatButtonModule } from '@angular/material/button';
 import { DialogModule } from 'primeng/dialog';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { FileUploadModule } from 'primeng/fileupload';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Frame } from '../../types/frame';
+import { WorkspaceService } from '../../services/workspace.service';
 
 @Component({
   selector: 'app-default-card',
   standalone: true,
-  imports: [DialogModule, MatButtonModule, SelectButtonModule, FileUploadModule, ReactiveFormsModule],
+  imports: [
+    DialogModule,
+    MatButtonModule,
+    SelectButtonModule,
+    FileUploadModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './default-card.component.html',
   styleUrl: './default-card.component.scss',
 })
 export class DefaultCardComponent {
   frameForm = this.buildForm();
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private workspaceService: WorkspaceService
+  ) {}
 
   buildForm() {
     return this.fb.group({
-      name: [''],
-      visibility: ['public'],
+      name: ['', Validators.required],
+      visibility: [1, Validators.required],
     });
   }
   stateOptions: any[] = [
-    { label: 'Particular', value: 'private' },
-    { label: 'Público', value: 'public' },
+    { label: 'Particular', value: 0 },
+    { label: 'Público', value: 1 },
   ];
 
   value: string = 'off';
@@ -39,7 +50,17 @@ export class DefaultCardComponent {
   }
 
   createBoard() {
-    // Lógica para criar um novo quadro
-    this.closeDialog();
+    if (this.frameForm.invalid) return;
+
+    this.workspaceService.createWorkspace(this.frameForm.value as Frame).subscribe({
+      next: (workspace: Frame) => {
+        console.log('Workspace created:', workspace);
+        this.closeDialog();
+        this.frameForm.reset({ visibility: 1 });
+      },
+      error: (error) => {
+        console.error('Error creating workspace:', error);
+      },
+    });
   }
 }
