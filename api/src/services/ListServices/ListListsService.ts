7681584a63528dc2143c6cee109c352a26dbe2d3
David <david.hcandido1@gmail.com>
2025-10-16 14:04:01 -0300

    fix(api): Consertar include para lists

--- api/src/services/ListServices/ListListsService.ts
+++ api/src/services/ListServices/ListListsService.ts
@@ -1,11 +1,24 @@
+import { IncludeOptions } from 'sequelize';
 import { List } from '../../models/List';
+import { Card } from '../../models/Card';
 
 export const ListListsService = async (
   workspaceId: number
 ): Promise<List[]> => {
+  let includeOptions: IncludeOptions[] = []
   const lists = await List.findAll({
     where: { workspaceId: workspaceId },
-    include: ['cards'],
+    include: includeOptions
   });
+
+  for (const list of lists) {
+    const cards = await Card.findAll({
+      where: { listId: list.id },
+    })
+
+    if(cards.length > 0) {
+      includeOptions.push({ model: Card, as: 'cards' })
+    }
+  }
   return lists;
 };
