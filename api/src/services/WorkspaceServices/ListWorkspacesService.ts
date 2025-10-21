13d1e474f553e5501f65cea13281b44bd3248050
David <david.hcandido1@gmail.com>
2025-10-21 15:38:21 -0300

    fix(api): corrigir lógica para listar workspaces

--- api/src/services/WorkspaceServices/ListWorkspacesService.ts
+++ api/src/services/WorkspaceServices/ListWorkspacesService.ts
@@ -1,14 +1,27 @@
 import { List } from '../../models/List';
 import { Workspace } from '../../models/Workspace';
-import { IncludeOptions } from 'sequelize';
+import { Op } from 'sequelize';
 
 export const ListWorkspacesServices = async (
   userId: number
 ): Promise<Workspace[]> => {
   const workspaces = await Workspace.findAll({
-    where: { ownerId: userId },
-    include: ['collaborators'],
+    where: {
+      [Op.or]: [
+        { ownerId: userId },
+        { '$collaborators.id$': userId },
+      ],
+    },
+    include: [
+      {
+        association: 'collaborators',
+        attributes: ['id', 'name', 'email'],
+        through: { attributes: [] },
+        required: false,
+      },
+      { model: List, as: 'lists', include: ['cards'] },
+    ],
+    order: [['createdAt', 'DESC']],
   });
-
   return workspaces;
 };
