5f31ef294359cd1287dbda23344f9c7150b654d9
Ana Caroll <ana2417carollina@gmail.com>
2025-10-22 17:03:46 -0300

    feat(front): completar task

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../../types/task';
 import { CommonModule } from '@angular/common';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CardService } from '../../services/card.service';
 
 @Component({
   selector: 'app-task',
@@ -12,6 +13,23 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit {
   @Input() task: Task | null = null;
+
+  checked: boolean = false;
+
+  constructor(private cardService: CardService) {}
+
+  ngOnInit() {
+    this.checked = this.task ? this.task.completed : false;
+  }
+
+  onCheckboxChange() {
+    if (this.task) {
+      this.task.completed = this.checked;
+    }
+    const newCard = { ...this.task };
+    console.log('Task completion changed:', newCard);
+    this.cardService.updateCard(this.task!.id, newCard as Task).subscribe();
+  }
 }
